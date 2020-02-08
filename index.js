3b6beb6b2c3d408fbc6db5dfc1f7fbf5e7f0d90f
amoodaa <amoodaa@gmail.com>
2020-02-08 18:18:15 +0200

    jsdocs for the function

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const read = require("./extractData");
 const writeSol = require("./writeOutput");
-
+/**
+ * @param {Object} input
+ * @param {number} input.maxSlices
+ * @param {number} input.typesOfPizzas
+ * @param {number[]} input.pizzaTypesSlices
+ */
 const callback = ({ maxSlices, typesOfPizzas, pizzaTypesSlices }) => {
   console.log({ maxSlices, typesOfPizzas, pizzaTypesSlices });
   let differentPizzaTypesToOrder = 0,
